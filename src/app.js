8395a94e3258f9d87f26e5a930b94d0c643798c5
Flaasks <flaviodipalma0@gmail.com>
2026-02-11 14:04:41 +0100

    CORS updated

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 import expenseRoutes from "./routes/expenseRoutes.js";
@@ -12,6 +13,7 @@ import notificationRoutes from './routes/notificationRoutes.js';
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoutes);
